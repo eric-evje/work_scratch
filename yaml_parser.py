eab20e055d6f4f91b9d387c0736873dbc0dccf9c
eric-evje <eric.evje@readcoor.com>
2018-11-02 19:13:37 -0400

    Found a newer version of this script floating around that contains the tolerance interval calculation

--- yaml_parser.py
+++ yaml_parser.py
@@ -3,39 +3,76 @@ import yaml
 import pandas as pd
 import scipy
 import numpy
+from numpy.random import seed
+from numpy.random import randn
+from numpy import mean
+from numpy import sqrt
+from scipy.stats import chi2
+from scipy.stats import norm
 
 
+
+def tolerance_interval(data, con=0.95, cov=0.95):
+    """
+    From https://machinelearningmastery.com/statistical-tolerance-intervals-in-machine-learning/
+    From https://www.itl.nist.gov/div898/handbook/prc/section2/prc263.htm
+    Calculates the 2-sided tolerance interval using Howe's method. Does not use Guenther's k2 correction.
+
+    params: 
+            data: array-like. normally distributed continuous data
+            con: level of confidence to achieve. Defaults to 0.95
+            cov: percent of population covered by the interval. Defaults to 0.95
+    Returns:
+            lower bound, upper bound
+    """
+
+    # specify degrees of freedom
+    n = len(data)
+    dof = n - 1
+    # specify data coverage
+    cov_inv = (1.0 - cov) / 2.0
+    gauss_critical = norm.isf(cov_inv)
+    #specify confidence
+    chi_critical = chi2.isf(q=con, df=dof)
+    # k_2
+    k_2 = gauss_critical * sqrt((dof * (1 + (1/n))) / chi_critical)
+    # summarize
+    data_mean = mean(data)
+    data_std = numpy.std(data)
+    lower, upper = data_mean-(k_2*data_std), data_mean+(k_2*data_std)
+    return (lower, upper)
 
 def cal_statistics(masses, pressures, dis_time, filename, reagent, valve):
 
     cols = ("filename", "reagent", "valve", "mass 1", "mass 2", "mass 3", "pressure 1", "pressure 2", "pressure 3", "L1", "L2",
-             "L3", "Lavg", "Lstdev", "L_cv_abs", "pass/fail")
+             "L3", "Lavg", "Lstdev", "L_cv_abs", "pass/fail", "lowerbound", "upperbound")
     stats_df = pd.DataFrame(columns=cols)
 
     vol_flow = []
+
+    # Convert mass and dispense time to volumetric flow rate of uL/s, assuming the calibration reagent is waster (i.e. specific gravity is ~1)
+    # Converts pressure to psi using transfer function developed by pressure sensor manufacturer https://sensing.honeywell.com/index.php%3Fci_id%3D45841
     for i in range(len(masses)):
         vol_flow.append(masses[i] / (dis_time / 1000.0)) # ul/s
         pressures[i] = ((pressures[i] - 1638) / (14745 - 1638)) * 14.5 # psi
-    # print(vol_flow)
-    # print(pressures)
 
+    # Compute Lohm for each dispense using equations developed by Lee Co https://www.theleeco.com/sites/leecompany/assets/efs-handbook/264/
     lohm = []
     for j in range(len(masses)):
         if vol_flow[j] != 0:
             lohm.append(scipy.sqrt(float(pressures[j])) / vol_flow[j])
         else:
             lohm.append(0)
-    # print(lohm)
+
+    tolerance_lower, tolerance_upper = tolerance_interval(lohm)
 
     lohm_avg = numpy.mean(lohm)
     lohm_stdev = numpy.std(lohm)
+
     if lohm_avg != 0:
         lohm_cv = numpy.abs((lohm_stdev / lohm_avg) * 100)
     else:
         lohm_cv = "NaN"
-    # print("lohm average: %f" % lohm_avg)
-    # print("stdev: %f" % lohm_stdev)
-    # print("percent_cv: %f" % lohm_cv)
 
     if lohm_cv > 2.0:
         pass_fail = False
@@ -43,21 +80,17 @@ def cal_statistics(masses, pressures, dis_time, filename, reagent, valve):
         pass_fail = True
 
     stats_df.loc[0] = (filename, reagent, valve, masses[0], masses[1], masses[2], pressures[0], pressures[1], pressures[2],
-                       lohm[0], lohm[1], lohm[2], lohm_avg, lohm_stdev, lohm_cv, pass_fail)
+                       lohm[0], lohm[1], lohm[2], lohm_avg, lohm_stdev, lohm_cv, pass_fail, tolerance_lower, tolerance_upper)
 
-    # print(stats_df)
     return stats_df
 
 if __name__ == "__main__":
 
-    # cols = ("filename", "reagent", "mass 1", "mass 2", "mass 3", "pressure 1", "pressure 2", "pressure 3", "L1", "L2",
-    #          "L3", "Lavg", "Lstdev", "L_cv_abs", "pass/fail")
     df = []
 
     for file in os.listdir('.'):
         if file.endswith(".yaml"):
             filename = file
-            # print("file: %s" % file)
             stream = open(file, 'r')
             input_map = yaml.load(stream)
             for j in range(1, 13):
@@ -66,18 +99,14 @@ if __name__ == "__main__":
                     pressures = []
                     reagent = input_map["reagents"]["valve_%d"%j]["short_name"]
                     valve = j
-                    # print("reagent: %s" % reagent)
                     dis_time = (float(input_map['reagents']['valve_%d' % j]['dispense_ms']))
-                    # print("dispense time: %d" % dis_time)
                     for i in range(1, 4):
                         masses.append(float(input_map['reagents']['valve_%d' % j][i]['grams']))
                         pressures.append(float(input_map['reagents']['valve_%d' % j][i]['mean_static_pressure']))
                     df.append(cal_statistics(masses, pressures, dis_time, filename, reagent, valve))
-                    # print(df)
                 except KeyError:
                     print("couldn't find valve %d in file %s" % (j, file))
 
-    print(df)
     df_output = pd.concat(df)
     filename = "results.csv"
     df_output.to_csv(filename, sep=',', index=False)
