2e52efcc5bc413c8212ab4e686d56cc77790783e
eric-evje <eric.evje@readcoor.com>
2019-07-18 23:38:06 -0400

    began adding full BOM functionality to be able to multiply order quantity by assembly

--- google_sheet_access.py
+++ google_sheet_access.py
@@ -51,10 +51,52 @@ def get_sheet_names(creds, service):
     values = result.get('values', [])
     tab_names = []
     for i in range(len(values)):
-        tab_names.append("'" + values[i][0] + "'!A:K")
+        tab_names.append(values[i][0])
+        
     # print(tab_names)
 
     return tab_names
+
+def scan_for_line_items(creds, service, tab):
+    parent = "RC-ASY" + tab
+    tab_name = "'" + tab + "'!A:K"
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    cols = ('PARENT', 'PARTNO', 'QTY')
+    
+    row_list = []
+    try:
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=tab_name).execute()
+        values = result.get('values', [])
+        for row in values:
+            try:
+                row.insert(0, parent)
+                row_list.append([row[0], row[1], row[3]])
+            except(IndexError):
+                # print("index error")
+                pass
+    except HttpError as e:
+        print("Couldn't find sheet{}".format(tab))
+        pass
+    row_list.append(['','',''])
+    df = pd.DataFrame(row_list, columns=cols)
+    df = clean_up_frame(df)
+    # print(df)   
+    return df
+
+def clean_up_frame(df):
+    # print(df)
+    BOM_start = 0
+    for index in range(0, int(len(df))):
+        print("value at index: {}".format(df["PARTNO"].iloc[index]))
+        if df["PARTNO"].iloc[index] == "PARTNO":
+            BOM_start = index + 1
+            break 
+    df = df[BOM_start:]
+    print("fixed df")
+    # print(df)
+    return df
 
 def scan_for_dates(creds, service, tab):
     # Call the Sheets API
@@ -102,19 +144,33 @@ if __name__ == '__main__':
 
     tab_names = get_sheet_names(creds, service)
 
-    order_df = []
+    full_BOM = []
     for tab in tab_names:
-        order_df.append(scan_for_dates(creds, service, tab))
+        full_BOM.append(scan_for_line_items(creds, service, tab))
 
-    BOM_df = pd.concat(order_df)
+    full_BOM_df = pd.concat(full_BOM)
     # print(order_df)
     
+    full_BOM_df['PARTNO'].replace('', np.nan, inplace=True)
+    full_BOM_df.dropna(subset=['PARTNO'], inplace=True)
 
-    BOM_df['PARTNO'].replace('', np.nan, inplace=True)
-    BOM_df.dropna(subset=['PARTNO'], inplace=True)
+    BOM_file_name = "BOM.csv" 
+    full_BOM_df.to_csv(BOM_file_name, sep=',', index=False)
 
-    print(BOM_df)
+
+    order_df = []
+    for tab in tab_names:
+        tab_name = "'" + tab + "'!A:K"
+        order_df.append(scan_for_dates(creds, service, tab_name))
+
+    weekly_BOM_df = pd.concat(order_df)
+    # print(order_df)
+    
+    weekly_BOM_df['PARTNO'].replace('', np.nan, inplace=True)
+    weekly_BOM_df.dropna(subset=['PARTNO'], inplace=True)
+
+    # print(weekly_BOM_df)
 
     stats_file_name = "Results.csv" 
-    BOM_df.to_csv(stats_file_name, sep=',', index=False)
+    weekly_BOM_df.to_csv(stats_file_name, sep=',', index=False)
     
