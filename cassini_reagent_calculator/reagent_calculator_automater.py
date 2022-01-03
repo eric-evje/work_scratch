4c7379506d1fcb2105bcfb43ff369310adee6777
ericevje <evje.eric@gmail.com>
2022-01-02 18:35:28 -0800

    Cleaned up cassini_reagent_calculator

--- cassini_reagent_calculator/reagent_calculator_automater.py
+++ cassini_reagent_calculator/reagent_calculator_automater.py
@@ -1,7 +1,6 @@
 '''
-Copyright (c) ReadCoor, Inc. 2019. All Rights Reserved.
-
-Order list generation script for Bio Kit Build Tracking
+Script employing Google Sheets API to automate analysis of reagent use
+calculations for Cassini
 '''
 
 from __future__ import print_function
@@ -21,16 +20,11 @@ import argparse
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-# The ID and range of a sample spreadsheet.
-# SPREADSHEET_ID = '1gGkE6kCS571NcA29B6psceN2jIOLUue-NVYQJiv3KGY' #Sr2.0 BOM
-# SPREADSHEET_ID = '1HDtEuJO9kTuiR1M8mG9s-hwDsXHXJcVFahQXsrDq_D0' #SR2.1 BOM
-SPREADSHEET_ID = '1pVIvUz61vQ8UA74E1ngY5_-LhBnqBePjn6H54iaMd2c' #RTF v1.0 Master mBOM & Traceability Template
+# The ID of READ spreadsheet.
+SPREADSHEET_ID = '1pVIvUz61vQ8UA74E1ngY5_-LhBnqBePjn6H54iaMd2c'
 
-
-# WRITE_SPREADSHEET_ID = '1It9ZOMteyUv6LCGQOmFZdXyMyJ2IjfA-LEfSC2rxxDA' #SR2.0 order list
-WRITE_SPREADSHEET_ID = '1pVIvUz61vQ8UA74E1ngY5_-LhBnqBePjn6H54iaMd2c' #SR2.1 LLI list
-# WRITE_RANGE = ["'1_wk'!A1", "'2_wk'!A:Z", "'3_wk'!A:Z", "'4_wk'!A:Z", 
-#             "'5_wk'!A:Z", "'6_wk'!A:Z","'7_wk'!A:Z", "'8_wk'!A:Z"]
+# The ID of WRITE spreadsheet.
+WRITE_SPREADSHEET_ID = '1pVIvUz61vQ8UA74E1ngY5_-LhBnqBePjn6H54iaMd2c'
 
 def credentials():
     # Gets credentials from google for accessing read/write access to sheets
@@ -58,10 +52,16 @@ def credentials():
     return(creds, service)
 
 def return_values(creds, service):
-    # Call the Sheets API
-    # Get the sheet names from the SheetNames sheet
+    '''
+    gets values from appropriate cell ranges to return as a single row dataframe to append to main df
+    and ultimately return as a single csv with results
+    '''
+    
+    #Create empty dataframe
     cols = ['AP', 'SP', 'Img', 'quencher', 'total_wells_req', 'plates_req', 'largest_reservoir', 'waste_mL']
     df = pd.DataFrame(columns=cols)
+
+    #Send get request via Google API for first range from spreadsheet and append to empty dataframe
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                                 range="'Reagent Calculator'!D57:D61").execute()
@@ -71,9 +71,9 @@ def return_values(creds, service):
     df['Img'] =                 wells[2]
     df['quencher'] =            wells[3]
     df['total_wells_req'] =     wells[4]
-
     df['plates_req'] = np.ceil(float(df['total_wells_req'].iloc[0]) / 96)  
 
+    #Send get request via Google API for second range from spreadsheet and appent to dataframe
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                                 range="'Reagent Calculator'!I20:I21").execute()
     reservoirs = result.get('values', [])
@@ -135,13 +135,17 @@ if __name__ == '__main__':
     df_conditions = pd.read_csv('reagent_calculator_input.csv', dtype='float64')
 
     for i in range(df_conditions.shape[0]):
+        # Write data from input spreadsheet to Google sheet
         esiv = df_conditions['esiv'].iloc[i]
         img_overshoot = df_conditions['img_overshoot'].iloc[i]
         well_plate = df_conditions['well_plate'].iloc[i]
         quencher_period = df_conditions['quencher_period'].iloc[i]
         interweaving=df_conditions['interweaving'].iloc[i]
         write_to_sheet(esiv=esiv, img_overshoot=img_overshoot, well_plate=well_plate, quencher_period=quencher_period, interweaving=interweaving)
+        # Sleeps are required to avoid going over reading and writing requests via API
         time.sleep(2)
+
+        # Read calculated values from Google sheet
         df_vals = return_values(creds, service)
         df_conditions['AP'].iloc[i] = df_vals['AP'].iloc[0]
         df_conditions['SP'].iloc[i] = df_vals['SP'].iloc[0]
@@ -151,113 +155,8 @@ if __name__ == '__main__':
         df_conditions['plates_req'].iloc[i] = df_vals['plates_req'].iloc[0]
         df_conditions['largest_reservoir'].iloc[i] = df_vals['largest_reservoir'].iloc[0]
         df_conditions['waste_mL'].iloc[i] = df_vals['waste_mL'].iloc[0]
+        # Sleeps are required to avoid going over reading and writing requests via API
         time.sleep(2)
 
+    # Output dataframe as csv for further analysis
     df_conditions.to_csv('reagent_calculator_input.csv', index=False)
-
-
-    # print("Did you remember to update the sheetnames list?")
-
-    # Argument parser
-    # parser = argparse.ArgumentParser(
-    #     description='This script extracts items and order quantities from the RC2 Master BOM.')
-    # parser.add_argument('builds', type=int,
-    #                     help='Number of builds to order for')
-    # parser.add_argument('update_full', type=int,
-    #                     help='Whether (1) or not (0) to update full bom sheet')
-    # parser.add_argument('deleted_assy_as_parts', type = int,
-    #                     help='Whether (1) or not (0) to delete assemblies in the PARTNO')
-    # args = parser.parse_args()
-
-    # Aquire Oauth credentials from google
-    
-
-    # write_to_sheet()
-    # return_values(creds, service)
-
-    # # Retrieve the tab names from the BOM spreadsheet
-    # tab_names = get_sheet_names(creds, service)
-
-    # # Retrieve the full BOM from the master BOM spreadsheet
-    # full_BOM = []
-    # for tab in tab_names:
-    #     time.sleep(0.5)
-    #     full_BOM.append(scan_for_line_items(creds, service, tab))
-
-    # # Clean up dataframe and drop ros with empty part numbers
-    # full_BOM_df = pd.concat(full_BOM, sort=False)
-    # # full_BOM_df['QTY'] = full_BOM_df['QTY'].astype(str)
-    # full_BOM_df['PARTNO'].replace('', np.nan, inplace=True)
-    # full_BOM_df.dropna(subset=['PARTNO'], inplace=True)
-    # full_BOM_df = full_BOM_df.loc[full_BOM_df['PARENT'] != full_BOM_df['PARTNO']]
-
-    # for i in range(len(full_BOM_df)):
-    #     string = full_BOM_df['PARTNO'].iloc[i][0:3]
-    #     prefixes = ['CSY', 'ASY', 'KTB']
-    #     if string in prefixes:
-    #         full_BOM_df['TYPE'].iloc[i] = 'ASSEMBLY'
-    #     else:
-    #         full_BOM_df['TYPE'].iloc[i] = 'COMPONENT'
-
-    # full_BOM_df = full_BOM_df[['OWNERSHIP', 'TYPE', 'PARENT', 'PARTNO',
-    #                             'DESCRIPTION', 'REV', 'QTY', 'UNITS',
-    #                             'VENDOR', 'VENDOR PARTNO', 'VENDOR LOT #',
-    #                             'MANUFACTURER', 'MANUF. PARTNO', 'PART STORAGE TEMP',
-    #                             'VOLUME PER KIT', 'VOLUME TO PREPARE', 'VOLUME PIPETTED INTO KIT',
-    #                             'FINAL CONCENTRATION', 'CSY QC PASS INITIALS',
-    #                             'PART KIT NAME', 'NOTES', 'MULTIPLIER',  
-    #                             'EXTENDED_QTY', 'APPROX. LEAD TIME [WEEKS]',   
-    #                             'COST EA.', 'EXT COST']]
-
-    # # Determine the order quantity based on the builds and quantities from the master bom
-    # order_df = order_quantity(full_BOM_df, args.builds)
-
-    # # Save the dataframe to a csv for posterity and debugging
-    # order_df.to_csv("BOM.csv") 
-
-
-    # order_df = order_df.reset_index()
-
-    # # Retrieve old order list from SR2.0 Order spreadsheet
-    # old_order_list_df = pull_old_order_list(creds, service)
-    # # Save for posterity and debugging
-    # old_order_list_df.to_csv("old.csv")
-
-    # # Merge the old and new order lists together.
-    # updated_df = merge_lists(order_df, old_order_list_df)
-
-    # # If directed, delete any assembly that is in the PARTNO column to clean up the ordering sheet 
-    # if args.deleted_assy_as_parts == 1:
-    #     m = ~updated_df['PARTNO'].str.contains('RC-ASY-\d+')
-    #     updated_df = updated_df[m]
-
-    # # Save for posterity and debugging
-    # # updated_df.to_csv("results.csv")
-
-    # # Get ready to send the merged order list back to the Google spreadsheet
-    # del updated_df["index"]
-    # full_order_list = updated_df.values.tolist()
-    # full_order_list.insert(0, ['OWNERSHIP', 'TYPE', 'PARENT', 'PARTNO',
-    #                             'DESCRIPTION', 'REV', 'QTY', 'UNITS',
-    #                             'VENDOR', 'VENDOR PARTNO', 'VENDOR LOT #',
-    #                             'MANUFACTURER', 'MANUF. PARTNO', 'PART STORAGE TEMP',
-    #                             'VOLUME PER KIT', 'VOLUME TO PREPARE', 'VOLUME PIPETTED INTO KIT',
-    #                             'FINAL CONCENTRATION', 'CSY QC PASS INITIALS',
-    #                             'PART KIT NAME', 'NOTES', 'MULTIPLIER',  
-    #                             'EXTENDED_QTY', 'APPROX. LEAD TIME [WEEKS]',   
-    #                             'COST EA.', 'EXT COST', 'MANUFACTURE RESPONSIBILITY', 'LOT #', 'QTY ORDERED', 
-    #                             'QTY RECEIVED', 'ORDERER', 'DATE ORDERED', 
-    #                             'EXP REC DATE', 'ORDER NOTES'])
-    # updated_df.to_csv("results.csv", index=False)
-    # update_time = time.strftime("%c")
-    # full_order_list.insert(0, ["Last updated: " + update_time])
-
-    # # If directed, update the SR2.0 order list with merged and updated order list
-    # if args.update_full == 1:
-    #     print("updating full BOM sheet")
-    #     write_to_sheet(full_order_list, "'Procurement - Build Tracking Sheet'!A1")
-
-    # print("Did you remember to update the sheetnames list?")
-
-
-
