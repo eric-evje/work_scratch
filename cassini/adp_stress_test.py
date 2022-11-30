6383689f65ec0efd84aefe1abe9fd261c3960743
Eric Evje <evje.eric@gmail.com>
2022-11-30 10:31:25 -0800

    Added retry logic to adp plunger command to catch faulty digital comms and separate from physical connection issues

--- cassini/adp_stress_test.py
+++ cassini/adp_stress_test.py
@@ -13,26 +13,25 @@ import numpy as np
 
 async def adp_test():
     fgc = FGC()
-
     await fgc.home_adp()
-    # await fgc.home_sipper()
-    # await fgc.home_gantry()
-    # await fgc.initialize_adp(linResolution_nmpS=31800)
-    # await fgc.set_gantry_x_position(400000000)
-    cycles = 50000
+    await fgc.home_sipper()
+    await fgc.home_gantry()
+    await fgc.initialize_adp(linResolution_nmpS=31800)
+    await fgc.set_gantry_x_position(400000000)
+    cycles = 32000
     check_cycles = 1000
     with open ("{}_adp_stress_test.txt".format(time.strftime("%Y%m%d_%H%M%S")), 'w') as f:
         f.write("time, cycle, z_pos_nm\n")
         for cycle in range(cycles):
             print("cycle {} of {}".format(cycle+1, cycles), end='\r')
             position = 110000000
-            await fgc.set_adp_position(position)
+            await fgc.set_adp_position(position, speed_nmps=100000000)
             await asyncio.sleep(0.1)
             new_line = "{}, {}, {}\n".format(time.time(), cycle, position)
             f.writelines(new_line)
 
             position = 0
-            await fgc.set_adp_position(position)
+            await fgc.set_adp_position(position, speed_nmps=100000000)
             await asyncio.sleep(0.1)
             new_line = "{}, {}, {}\n".format(time.time(), cycle, position)
             f.writelines(new_line)
@@ -44,31 +43,35 @@ async def adp_test():
         return
 
 async def check_connectivity(cycle):
+    steps = np.arange(0, 110000000, 500000)
+    await adp_cycle(cycle, steps)
+
+    steps = np.flip(np.arange(0, 110000000-250000, 500000))
+    await adp_cycle(cycle, steps)
+
+    return
+
+async def adp_cycle(cycle, steps):
     fgc = FGC()
     plunger_positions = [100, 110]
 
-    steps = np.arange(0, 110000000, 500000)
     for i, step in enumerate(steps):
         await fgc.set_adp_position(step)
-        try:
-            await fgc.adp_absolute_position(200, plunger_positions[i%2], 0)
-        except Exception as error:
-            print(error)
-            print("Failed after {} cycles".format(cycle))
-            raise
-
-    steps = np.flip(np.arange(0, 110000000-250000, 500000))
-    for i, step in enumerate(steps):
-        await fgc.set_adp_position(step)
-        try:
-            await fgc.adp_absolute_position(200, plunger_positions[i%2], 0)
-        except Exception as error:
-            print(error)
-            print("Failed after {} cycles".format(cycle))
-            raise
-
+        tries = 3
+        for j in range(tries):
+            try:
+                await fgc.adp_absolute_position(200, plunger_positions[i%2], 0)
+            except Exception as error:
+                print(error)
+                if j < tries - 1:
+                    print("retrying {} more times".format(tries - j + 1))
+                    asyncio.sleep(1)
+                    continue
+                else:
+                    print("Failed after {} cycles".format(cycle))
+                    raise
+            break
     return
-        
 
 
 async def main():
